d9e620008e402e0c48bed069dfea6034175289c9
Alen Reji <2460290r@student.gla.ac.uk>
2021-04-03 14:58:05 +0100

    views

--- foodieapp/views.py
+++ foodieapp/views.py
@@ -1,6 +1,10 @@
 from django.shortcuts import render
-
-# Create your views here.
+from django.contrib.auth import authenticate, login
+from django.http import HttpResponse
+from django.urls import reverse
+from django.shortcuts import redirect
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth import logout
 
 def home(request):
     # Construct a dictionary to pass to the template engine as its context.
@@ -10,3 +14,50 @@ def home(request):
     # We make use of the shortcut function to make our lives easier.
     # Note that the first parameter is the template we wish to use.
     return render(request, 'foodie/home.html', context=context_dict)
+    
+def register(request):
+    registered = False
+    
+    if request.method == 'POST':
+        user_form = UserProfileForm(request.POST)
+        
+        if user_form.is_valid():
+            user = user_form.save()
+            user.set_password(user.password)
+            user.save()
+            registered = True
+        else:
+            print(user_form.errors)
+    else:ÓÍ
+        user_form = UserForm()
+
+    
+    return render(request, 'foodie/signup.html', context = {'user_form': user_form, 
+                                                            'registered': registered})
+
+def user_login(request):
+
+    if request.method == 'POST':
+
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+        user = authenticate(username=username, password=password)
+
+        if user:
+            if user.is_active:
+                login(request, user)
+                return redirect(reverse('foodie:home'))
+            else:
+                return HttpResponse("Your account is disabled.")
+        else:
+            print(f"Invalid login details: {username}, {password}")
+            return HttpResponse("Invalid login details supplied.")
+            
+    else:
+        return render(request, 'foodie/login.html')
+
+@login_required
+def user_logout(request):
+    logout(request)
+    return redirect(reverse('foodie:home'))
+
