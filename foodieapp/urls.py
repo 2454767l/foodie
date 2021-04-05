63a03e82d070d28dabae42f57417e34566b8f71c
Thomas Bidaut <2473061b@student.gla.ac.uk>
2021-04-05 16:56:06 +0100

    search view

--- foodieapp/urls.py
+++ foodieapp/urls.py
@@ -5,6 +5,7 @@ app_name = 'foodieapp'
 
 urlpatterns = [
     path('', views.home, name='home'),
+    path('search/', views.search, name='search'),
     path('register/', views.register, name='register'),
     path('login/', views.user_login, name='login'),
     path('logout/', views.user_logout, name='logout'),
--- foodieapp/views.py
+++ foodieapp/views.py
@@ -6,9 +6,10 @@ from django.shortcuts import redirect
 from django.contrib.auth.decorators import login_required
 from django.contrib.auth import logout
 from django.contrib.auth.models import User
-from foodieapp.models import UserProfile
+from foodieapp.models import UserProfile, Recipe
 from foodieapp.forms import UserForm
 from django.views.generic import View
+
 
 def home(request):
     highest_voted = Recipe.objects.orderby(-upvotes)[:5]
@@ -16,16 +17,28 @@ def home(request):
     context_dict['highest'] = highest_voted
     return render(request, 'foodie/home.html', context=context_dict)
 
+
 def search(request):
     query = request.GET['query']
-    return render()
+    query_filter = request.GET['filter']
+    
+    if query_filter == "title":
+        results = Recipe.objects.filter(query in Recipe.title)
+    elif query_filter == "user":    
+        results = Recipe.objects.filter(Recipe.user == query)
+    elif query_filter == "ingredients":
+        results = Recipe.objects.filter(query in Recipe.ingredients)
+    
+    return render(request, 'search.html', results)
+
 
 def recipes(request):
     all_recipes = Recipe.objects
-    context_dict= {}
+    context_dict = {}
     context_dict['all'] = all_recipes
 
     return render(request, 'foodie/allRecipes.html', context=context_dict)
+
 
 def show_recipe(request, recipe_name_slug):
     context_dict = {}
@@ -38,11 +51,12 @@ def show_recipe(request, recipe_name_slug):
 
     return render(request, 'foodie/recipe.html', context=context_dict)
 
+
 @login_required
 def new_recipe(request):
     form = RecipeForm()
 
-    if request.method=='POST':
+    if request.method == 'POST':
         form = RecipeForm(request.POST)
 
         if form.is_valid:
@@ -51,14 +65,15 @@ def new_recipe(request):
         else:
             print(form.errors)
 
-    return render(request, 'foodie/newrecipe.html', {'form':form})
-    
+    return render(request, 'foodie/newrecipe.html', {'form': form})
+
+
 def register(request):
     registered = False
-    
+
     if request.method == 'POST':
         user_form = UserForm(request.POST)
-        
+
         if user_form.is_valid():
             user = user_form.save()
             user.set_password(user.password)
@@ -69,9 +84,9 @@ def register(request):
     else:
         user_form = UserForm()
 
-    
-    return render(request, 'foodie/register.html', context = {'user_form': user_form,
+    return render(request, 'foodie/register.html', context={'user_form': user_form,
                                                             'registered': registered})
+
 
 def user_login(request):
 
@@ -90,22 +105,22 @@ def user_login(request):
         else:
             print(f"Invalid login details: {username}, {password}")
             return HttpResponse("Invalid login details supplied.")
-            
+
     else:
         return render(request, 'foodie/login.html')
+
 
 @login_required
 def user_logout(request):
     logout(request)
     return redirect(reverse('foodie:home'))
 
+
 class LikeRecipe(View):
     def get(self, request):
         recipe_id = request.GET['recipe.id']
-        recipe = recipe.objects.get(id = int(recipe_id))
+        recipe = recipe.objects.get(id=int(recipe_id))
         recipe.upvotes += 1
         recipe.save()
 
         return HttpResponse(recipe.upvotes)
-
-
