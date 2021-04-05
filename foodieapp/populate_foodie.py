2b3b5ff7a5e1401732b899696779f96e26646791
Thomas Bidaut <2473061b@student.gla.ac.uk>
2021-04-05 22:26:41 +0100

    broken populate 2.0

--- foodieapp/populate_foodie.py
+++ foodieapp/populate_foodie.py
@@ -6,43 +6,49 @@ os.environ.setdefault('DJANGO_SETTINGS_MODULE',
 
 django.setup()
 
+
 def populate():
     # create users from dictionaries,
     # then create recipes linked to them,
     # then create ratings
-    users = [{'username':'ben', 'firstname':'ben', 'lastname':'bennison', 'password':'1234' }]
+    users = [{'username': 'ben', 'firstname': 'ben',
+              'lastname': 'bennison', 'password': '1234'}]
 
-    recipes = [{'title': 'Pizza', 'photo': '',
+    recipes = [{'title': 'Pizza', 'photo': '', 'user': 'alen',
                 'description': 'Turn on oven. Put pizza in. Wait 30 mins.',
-                'dietPref': 'Vegetarian', 'difficulty': '2', 
+                'dietPref': 'Vegetarian', 'difficulty': '2',
                 'ingredients': 'flour, oil, water, tomato sauce, cheese'},
-                {'title': 'Pasta', 'photo': '',
 
+               {'title': 'Pasta', 'photo': '', 'user': 'ben',
                 'description': 'Boil water. Put pasta in. Wait 10 mins.',
-                'dietPref': 'Vegan', 'difficulty': '1', 
+                'dietPref': 'Vegan', 'difficulty': '1',
                 'ingredients': 'dried pasta'},
 
-                {'title': 'Salad', 'photo': '',
+               {'title': 'Salad', 'photo': '', 'user': 'erin',
                 'description': 'Wash and chop up veg. Toss in a bowl. Serve with vinagrette',
-                'dietPref': 'Vegan', 'difficulty': '1', 
+                'dietPref': 'Vegan', 'difficulty': '1',
                 'ingredients': 'salad, onions, tomatoes, carrots, peppers'}]
 
-    ratings = [{'user':'alen', 'recipe':'pizza'},
-                {'user':'ben', 'recipe':'pizza'}, 
-                {'user':'erin', 'recipe':'pizza'},
-                {'user':'thomas', 'recipe':'pizza'},
+    ratings = [{'user': 'alen', 'recipe': 'pizza'},
+               {'user': 'ben', 'recipe': 'pizza'},
+               {'user': 'erin', 'recipe': 'pizza'},
+               {'user': 'thomas', 'recipe': 'pizza'},
 
-                {'user':'alen', 'recipe':'pasta'},
-                {'user':'ben', 'recipe':'pasta'}, 
-                {'user':'erin', 'recipe':'pasta'},
-                {'user':'thomas', 'recipe':'pasta '},
+               {'user': 'alen', 'recipe': 'pasta'},
+               {'user': 'ben', 'recipe': 'pasta'},
+               {'user': 'erin', 'recipe': 'pasta'},
+               {'user': 'thomas', 'recipe': 'pasta '},
 
-                {'user':'alen', 'recipe':'salad'},
-                {'user':'ben', 'recipe':'salad'},
-                {'user':'thomas', 'recipe':'salad'}
-                ]
+               {'user': 'alen', 'recipe': 'salad'},
+               {'user': 'ben', 'recipe': 'salad'},
+               {'user': 'thomas', 'recipe': 'salad'}
+               ]
 
+
+def add_recipe(title, photo, user, description, dietPref, difficulty, ingredients):
+    recipe = Recipe.objects.get_or_create(title=title, photo=photo, user=user, description=description,
+                                          dietPref=dietPref, difficulty=difficulty, ingredients=ingredients)
     
 
-
-    
+def add_rating(user, recipe):
+    rating = Rating.objects.get_or_create(user)
