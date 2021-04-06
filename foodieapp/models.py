48315177a94f251a94f17240e728a6c47530745e
Alen Reji <2460290r@student.gla.ac.uk>
2021-04-06 14:45:46 +0100

    working recipe page

--- foodieapp/models.py
+++ foodieapp/models.py
@@ -2,6 +2,7 @@ from django.db import models
 from django.contrib.auth.models import User
 from datetime import datetime, date
 from django.utils import timezone
+from django.template.defaultfilters import slugify
 
 class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
@@ -20,7 +21,15 @@ class Recipe(models.Model):
     upvotes = models.IntegerField
     difficulty = models.IntegerField
     ingredients = models.CharField(max_length=1000)
+    slug = models.SlugField(unique=True)
     
+    def save(self, *args, **kwargs):
+        self.slug = slugify(self.title)
+        super(Recipe, self).save(*args, **kwargs)
+        
+    class Meta:
+        verbose_name_plural = 'recipes'
+
     def __str__(self):
         return self.title
 
--- foodieapp/urls.py
+++ foodieapp/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
     path('logout/', views.user_logout, name='logout'),
     path('like_recipe/', views.LikeRecipe.as_view(), name='upvote_recipe'),
     path('recipes/', views.recipes, name='recipes'),
-    path('recipes/<slug:recipe_name_slug>/', views.show_recipe, name='show_recipe'),
+    path('recipes/<slug:recipe_title_slug>/', views.show_recipe, name='show_recipe'),
     path('login/account/', views.show_account, name='show_account'),
     path('login/account/newrecipe/', views.new_recipe, name='new_recipe'),
 ]
